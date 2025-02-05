e889dc67066a13932b9d8a6c974de79dbdef7126
MunSeongyun <borygashill608@gmail.com>
2025-02-05 09:43:27 +0900

    🐛 fix : banners.controller의 post 부분의 swagger api에서 필드명이 image여야 하는데 filer로 되어있는 부분분 수정

--- api/src/banners/banners.controller.ts
+++ api/src/banners/banners.controller.ts
@@ -17,33 +17,44 @@ import { CreateBannerDto } from './dto/create-banner.dto';
 import { UpdateBannerDto } from './dto/update-banner.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ImageDiskOptions } from 'src/common/multer-imageDiskoptions';
-import { ApiBody, ApiConsumes, ApiExcludeController, ApiOperation, ApiParam, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiExcludeController,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('Banner')
 @Controller('banners')
 export class BannersController {
   constructor(private readonly bannersService: BannersService) {}
 
-  @ApiOperation({summary:'배너 작성하기'})  
+  @ApiOperation({ summary: '배너 작성하기' })
   @ApiConsumes('multipart/form-data')
   @ApiBody({
-    schema:{
-      type:'object',
-      properties:{
-        file:{
-          type:'string',
-          format:'binary',
-          description:'배너 이미지, 필수'
+    schema: {
+      type: 'object',
+      properties: {
+        image: {
+          type: 'string',
+          format: 'binary',
+          description: '배너 이미지, 필수',
         },
-        url:{type:'string'},
-        expiredDate:{type:'Date'},
-        language:{type:'string'},
-      }
-    }
+        url: { type: 'string' },
+        expiredDate: { type: 'Date' },
+        language: { type: 'string' },
+      },
+    },
   })
-  @ApiResponse({example:{
-    message:'배너가 작성되었습니다.'
-  }})
+  @ApiResponse({
+    example: {
+      message: '배너가 작성되었습니다.',
+    },
+  })
   @Post()
   @UseInterceptors(FileInterceptor('image', ImageDiskOptions))
   async create(
@@ -51,75 +62,79 @@ export class BannersController {
     @UploadedFile() file: Express.Multer.File,
   ) {
     await this.bannersService.create(createBannerDto, file);
-    return {message:'배너가 작성되었습니다.'}
+    return { message: '배너가 작성되었습니다.' };
   }
 
-  @ApiOperation({summary:'배너 받아오기'})
+  @ApiOperation({ summary: '배너 받아오기' })
   @ApiQuery({
-    name:'ignore',
-    example:false,
-    description:'만료기간(expiredDate)가 지난 글을 불러올지, true면 전부 불러옴, default:false',
-    required:false
+    name: 'ignore',
+    example: false,
+    description:
+      '만료기간(expiredDate)가 지난 글을 불러올지, true면 전부 불러옴, default:false',
+    required: false,
   })
   @ApiResponse({
-    example:{
-      message:'배너를 불러왔습니다.',
-      data:[]
-    }
+    example: {
+      message: '배너를 불러왔습니다.',
+      data: [],
+    },
   })
   @Get()
-  async findAll(@Query('ignore',new DefaultValuePipe(false)) ignore:boolean) {
+  async findAll(@Query('ignore', new DefaultValuePipe(false)) ignore: boolean) {
     return await this.bannersService.findAll(ignore);
   }
 
-  @ApiOperation({summary:'배너 수정하기'})
+  @ApiOperation({ summary: '배너 수정하기' })
   @ApiBody({
-    schema:{
-      type:'object',
-      properties:{
-        image:{
-          type:'string',
-          format:'binary',
-          description:'배너 이미지'
+    schema: {
+      type: 'object',
+      properties: {
+        image: {
+          type: 'string',
+          format: 'binary',
+          description: '배너 이미지',
         },
-        expiredDate:{type:'Date',description:'만료일자'},
-        url:{type:'string',description:'이미지 클릭시 이동할 url'},
-        language:{type:'string',description:'이미지가 표시될 사이트 언어'},
-      }
-    }
+        expiredDate: { type: 'Date', description: '만료일자' },
+        url: { type: 'string', description: '이미지 클릭시 이동할 url' },
+        language: {
+          type: 'string',
+          description: '이미지가 표시될 사이트 언어',
+        },
+      },
+    },
   })
   @ApiResponse({
-    example:{
-      message:'배너가 수정되었습니다.'
-    }
+    example: {
+      message: '배너가 수정되었습니다.',
+    },
   })
   @ApiConsumes('multipart/form-data')
   @Patch(':id')
-  @UseInterceptors(FileInterceptor('image',ImageDiskOptions))
+  @UseInterceptors(FileInterceptor('image', ImageDiskOptions))
   async update(
     @Param('id') id: number,
     @Body() updateBannerDto: UpdateBannerDto,
-    @UploadedFile() file:Express.Multer.File
+    @UploadedFile() file: Express.Multer.File,
   ) {
-    await this.bannersService.update(id, updateBannerDto,file);
-    return {message:'배너가 수정되었습니다.'}
+    await this.bannersService.update(id, updateBannerDto, file);
+    return { message: '배너가 수정되었습니다.' };
   }
 
-  @ApiOperation({summary:'배너 삭제하기'})
+  @ApiOperation({ summary: '배너 삭제하기' })
   @ApiParam({
-    name:'id',
-    example:1
+    name: 'id',
+    example: 1,
   })
   @ApiResponse({
-    example:{
-      message:'배너가 삭제되었습니다.'
-    }
+    example: {
+      message: '배너가 삭제되었습니다.',
+    },
   })
   @Delete(':id')
   async remove(@Param('id') id: number) {
     await this.bannersService.remove(id);
     return {
-      message:'배너가 삭제되었습니다.'
-    }
+      message: '배너가 삭제되었습니다.',
+    };
   }
 }
