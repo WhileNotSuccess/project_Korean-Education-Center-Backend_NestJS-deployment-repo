ed737c9e00cdf457164ee47e44c5a3eb48edcb61
MunSeongyun <borygashill608@gmail.com>
2025-02-16 12:59:15 +0900

    🐛fix: load username and attached files with application form
    Modified the application form retrieval process to include the username and attached files.
    Also, fixed an issue where `isDone` in the application form could not be updated due to formData not supporting boolean values.
    
    application-form을 불러올 때, 유저명과 첨부파일을 함께 불러오게 수정. formdata에 boolean값을 넣을 수 없어서 application-form의 isDone을 업데이트 하지 못하는 문제 해결

--- api/src/application-form/application-form.controller.ts
+++ api/src/application-form/application-form.controller.ts
@@ -16,7 +16,15 @@ import { CreateApplicationFormDto } from './dto/create-application-form.dto';
 import { UpdateApplicationFormDto } from './dto/update-application-form.dto';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { FileDiskOptions } from 'src/common/multer-fileDiskOptions';
-import { ApiBody, ApiConsumes, ApiOperation, ApiParam, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('ApplicationForm')
 @Controller('application-form')
@@ -25,115 +33,139 @@ export class ApplicationFormController {
     private readonly applicationFormService: ApplicationFormService,
   ) {}
 
-  @ApiOperation({summary:'입학 신청 접수'})
+  @ApiOperation({ summary: '입학 신청 접수' })
   @ApiBody({
-    schema:{
-      type:'object',
-      properties:{
-        files:{
-          type:'string',
-          format:'binary',
-          description:'입학 신청서류'
+    schema: {
+      type: 'object',
+      properties: {
+        files: {
+          type: 'string',
+          format: 'binary',
+          description: '입학 신청서류',
         },
-        course:{type:'string',description:'신청 전공'}
-      }
-    }
-  })  
+        course: { type: 'string', description: '신청 전공' },
+      },
+    },
+  })
   @ApiConsumes('multipart/form-data')
   @ApiResponse({
-    example:{
-      message:'입학신청이 접수되었습니다.'
-    }
+    example: {
+      message: '입학신청이 접수되었습니다.',
+    },
   })
   @Post()
-  @UseInterceptors(FilesInterceptor('files',10,FileDiskOptions))
+  @UseInterceptors(FilesInterceptor('files', 10, FileDiskOptions))
   async createApplication(
-    @Body() CreateApplicationFormDto:CreateApplicationFormDto,
-    @UploadedFiles() files:Express.Multer.File[]
-  ){
-    const userId=1 // guard 적용 후 삭제
-    await this.applicationFormService.create(CreateApplicationFormDto,files,userId)
-    return {message:'입학신청이 접수되었습니다.'}
+    @Body() CreateApplicationFormDto: CreateApplicationFormDto,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    const userId = 1; // guard 적용 후 삭제
+    await this.applicationFormService.create(
+      CreateApplicationFormDto,
+      files,
+      userId,
+    );
+    return { message: '입학신청이 접수되었습니다.' };
   }
 
-  @ApiOperation({summary:'입학 신청 불러오기'})
+  @ApiOperation({ summary: '입학 신청 불러오기' })
   @ApiQuery({
-    name:'ignore',
-    example:false,
-    description:'이미 처리된(isDone이 true) 신청은 제외하고 불러올지, true면 전부 불러옴, defalut:false',
-    required:false
+    name: 'ignore',
+    example: false,
+    description:
+      '이미 처리된(isDone이 true) 신청은 제외하고 불러올지, true면 전부 불러옴, defalut:false',
+    required: false,
   })
-  @ApiQuery({name:'limit',example:10,default:10,required:false})
-  @ApiQuery({name:'page',example:1,default:1,required:false})
+  @ApiQuery({ name: 'limit', example: 10, default: 10, required: false })
+  @ApiQuery({ name: 'page', example: 1, default: 1, required: false })
   @ApiResponse({
-    example:{
-      message:'입학신청의 1번째 페이지를 불러왔습니다.',
-      data:[
+    example: {
+      message: '입학신청의 1번째 페이지를 불러왔습니다.',
+      data: [
         {
-          id:1,
-          userId:1,
-          course:'korean',
-          createdDate:'2025-01-31T15:12:47.145Z',
-          isDone:false
-        }
+          id: 1,
+          userId: 1,
+          course: 'korean',
+          createdDate: '2025-01-31T15:12:47.145Z',
+          isDone: false,
+          attachments: [
+            {
+              id: 1,
+              applicationId: 1,
+              filename: 'string',
+              fileSize: 45621,
+              filetype: 'string',
+            },
+          ],
+        },
       ],
-      currentPage:1,
-      prevPage:null,
-      nextPage:null,
-      totalPage:1
-    }
+      currentPage: 1,
+      prevPage: null,
+      nextPage: null,
+      totalPage: 1,
+    },
   })
   @Get()
   async getPagination(
-    @Query('limit',new DefaultValuePipe(10)) limit:number,
-    @Query('page', new DefaultValuePipe(1)) page:number,
-    @Query('ignore',new DefaultValuePipe(false)) ignore:boolean
-  ){
-    return await this.applicationFormService.findPagination(limit,page,ignore)
+    @Query('limit', new DefaultValuePipe(10)) limit: number,
+    @Query('page', new DefaultValuePipe(1)) page: number,
+    @Query('ignore', new DefaultValuePipe(false)) ignore: boolean,
+  ) {
+    return await this.applicationFormService.findPagination(
+      limit,
+      page,
+      ignore,
+    );
   }
 
-
-  @ApiOperation({summary:'입학 신청 수정'})
+  @ApiOperation({ summary: '입학 신청 수정' })
   @ApiBody({
-    schema:{
-      type:'object',
-      properties:{
-        files:{
-          type:'string',
-          format:'binary',
-          description:'새로 추가하는 입학 신청서류, 서류가 없을 경우 필수 아님'
+    schema: {
+      type: 'object',
+      properties: {
+        files: {
+          type: 'string',
+          format: 'binary',
+          description:
+            '새로 추가하는 입학 신청서류, 서류가 없을 경우 필수 아님',
         },
-        course:{type:'string',description:'신청 전공, 필수 아님'},
-        isDone:{type:'boolean',description:'처리 여부, 필수 아님'},
-        deleteFilePath:{type:'string',description:'삭제할 파일 이름 배열을 string형태로 한 것'}
-      }
-    }
+        course: { type: 'string', description: '신청 전공, 필수 아님' },
+        isDone: { type: 'boolean', description: '처리 여부, 필수 아님' },
+        deleteFilePath: {
+          type: 'string',
+          description: '삭제할 파일 이름 배열을 string형태로 한 것',
+        },
+      },
+    },
   })
   @ApiConsumes('multipart/form-data')
   @ApiResponse({
-    example:{message:'입학 신청이 수정되었습니다.'}
+    example: { message: '입학 신청이 수정되었습니다.' },
   })
   @Patch(':id')
-  @UseInterceptors(FilesInterceptor('files',10,FileDiskOptions))
+  @UseInterceptors(FilesInterceptor('files', 10, FileDiskOptions))
   async updateApplication(
-    @Param('id') id:number,
-    @UploadedFiles() files:Express.Multer.File[], // 새로 추가되는 파일만 받음
-    @Body() UpdateApplicationFormDto:UpdateApplicationFormDto
-  ){
-    await this.applicationFormService.update(id,UpdateApplicationFormDto,files)
-    return {message:'입학 신청이 수정되었습니다.'}
-  } 
+    @Param('id') id: number,
+    @UploadedFiles() files: Express.Multer.File[], // 새로 추가되는 파일만 받음
+    @Body() updateApplicationFormDto: UpdateApplicationFormDto,
+  ) {
+    console.log(updateApplicationFormDto);
+    await this.applicationFormService.update(
+      id,
+      updateApplicationFormDto,
+      files,
+    );
+    return { message: '입학 신청이 수정되었습니다.' };
+  }
 
-  @ApiOperation({summary:'입학신청 취소'})
-  @ApiParam({name:'id',example:1})
+  @ApiOperation({ summary: '입학신청 취소' })
+  @ApiParam({ name: 'id', example: 1 })
   @ApiResponse({
-    example:{message:'입학신청이 취소되었습니다.'}
+    example: { message: '입학신청이 취소되었습니다.' },
   })
   @Delete(':id')
-  async deleteApplication(
-    @Param('id') id:number
-  ){
-    await this.applicationFormService.remove(id)
-    return {message:'입학신청이 취소되었습니다.'}
+  async deleteApplication(@Param('id') id: number) {
+    await this.applicationFormService.remove(id);
+    return { message: '입학신청이 취소되었습니다.' };
   }
 }
--- api/src/application-form/dto/update-application-form.dto.ts
+++ api/src/application-form/dto/update-application-form.dto.ts
@@ -2,11 +2,13 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateApplicationFormDto } from './create-application-form.dto';
 import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
-export class UpdateApplicationFormDto extends PartialType(CreateApplicationFormDto) {
-    @IsBoolean()
-    @IsOptional()
-    isDone?:boolean
-    @IsString()
-    @IsOptional()
-    deleteFilePath:string
+export class UpdateApplicationFormDto extends PartialType(
+  CreateApplicationFormDto,
+) {
+  @IsString()
+  @IsOptional()
+  isDone?: string;
+  @IsString()
+  @IsOptional()
+  deleteFilePath: string;
 }
